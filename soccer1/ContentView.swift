823cd76576865a66cce2187d13ad9f277d4bf558
Sam Tawfik <sam.tawfik@icloud.com>
2020-03-25 22:57:04 -0700

    Initial Commit

---
+++ soccer1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  soccer1
+//
+//  Created by sam on 3/25/20.
+//  Copyright © 2020 Cabohut. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
